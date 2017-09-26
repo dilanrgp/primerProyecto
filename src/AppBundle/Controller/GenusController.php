a0409594f312c2fd1d16ac77d95d3d83c65ec9cc
Dilan Gonzalez <dilanrgp@gmail.com>
2017-09-26 14:40:12 -0430

    add templating

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,17 +4,21 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 
 
-class GenusController //extends Controller
+class GenusController extends Controller
 {
     /**
-     * @Route("/genus")
+     * @Route("/genus/{genusName}")
      */
-    public function showAction()
+    public function showAction($genusName)
     {
-        return new Response('Hola Mundo');
+        //return new Response('Hola '.$genusName.' Mundo');
+        $templating = $this->container->get('templating');
+        $html = $templating->render('genus/show.html.twig', array('name'=>$genusName));
+        return new Response($html);
     }
 }
